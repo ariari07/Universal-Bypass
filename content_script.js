d334e87554429020682cb8e9e4432653531b93ad
ariari07 <48227139+ariari07@users.noreply.github.com>
2019-03-05 06:15:53 +0700

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -537,44 +537,6 @@ if(document instanceof HTMLDocument)
 			if(document.querySelector(".captcha_loader .progress-bar"))
 				document.querySelector(".captcha_loader .progress-bar").setAttribute("aria-valuenow","100")
 		})
-		domainBypass("stealive.club",()=>{
-			if(document.getElementById("counter"))
-				document.getElementById("counter").innerHTML="0"
-		})
-		hrefBypass(/((binerfile|pafpaf)\\.info)|(kurosafety\\.menantisenja\\.com)/,()=>{//KuroSafe
-			let b=document.querySelector("#mybutton[href]")
-			if(b)
-				safelyNavigate(b.href)
-		})
-		domainBypass("gotoo.loncat.in",()=>{
-			let a=document.querySelector("a[href^='http://gotoo.loncat.in/go.php?open=']")
-			if(a)
-				safelyNavigate(a.href)
-		})
-		domainBypass("idnation.net",()=>{
-			let b=document.querySelector("#linko[href]")
-			if(b)
-				safelyNavigate(b.href)
-		})
-		domainBypass("mazika2day.com",()=>{
-			let b=document.querySelector(".linkbtn[href]")
-			if(b)
-				safelyNavigate(b.href)
-		})
-		domainBypass("ux9.de",()=>{
-			let m=document.querySelector("meta[http-equiv='refresh'][content]")
-			if(m&&m.content.indexOf(";url=http"))
-			{
-				safelyNavigate(m.content.split(";url=")[1])
-			}
-		})
-		domainBypass("telolet.in",()=>{
-			let b=document.querySelector("a#skip[href]")
-			if(!b)
-				b=document.querySelector(".redirect_url > a[href]")
-			if(b)
-				safelyNavigate(b.href)
-		})
 		domainBypass("vipdirect.cc",()=>{
 			if(typeof ab=="number"&&typeof asdf=="function")
 			{
